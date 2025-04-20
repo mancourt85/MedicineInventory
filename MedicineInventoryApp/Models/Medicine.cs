ce7d4d8f04ed5f8a898a7f7ef75e2c64e6e2147c
Manuel <--replace-all>
2025-04-20 17:29:59 -0500

    Base API finished

---
+++ MedicineInventoryApp/Models/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicineInventoryApp.Models
+{
+    public class Medicine
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+
+
+}
---
+++ MedicineInventoryApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using MedicineInventoryApp.Data;
+using MedicineInventoryApp.Interfaces;
+using MedicineInventoryApp.Interfaces.Repositories;
+using MedicineInventoryApp.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MedicineInventoryApp.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<InventoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(); 
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Medicines}/{action=Index}/{id?}");
+
+await app.RunAsync();
